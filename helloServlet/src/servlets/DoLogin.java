9e247798af9f53f20ba7d9d5b60824452aa46e7a
binssa <bin.sa0823@gmail.com>
2021-07-11 18:08:59 +0900

    First Dynamic Web With Servlet

---
+++ helloServlet/src/servlets/DoLogin.java
@@ -0,0 +1,41 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DoLogin
+ */
+@WebServlet("/DoLogin")
+public class DoLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DoLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Read Form Data
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		// perform business logic
+		PrintWriter out = response.getWriter();
+		out.println("UserName : " + username);
+		out.println("Password : " + password );
+	}
+
+}
