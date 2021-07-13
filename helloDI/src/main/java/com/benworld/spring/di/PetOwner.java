7ba95e67c7a87970c154ccc9bed802dede4ce0c5
binssa <bin.sa0823@gmail.com>
2021-07-14 07:04:02 +0900

    Dependency Injection Study using Annotation

--- helloDI/src/main/java/com/benworld/spring/di/PetOwner.java
+++ helloDI/src/main/java/com/benworld/spring/di/PetOwner.java
@@ -1,14 +1,22 @@
 package com.benworld.spring.di;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class PetOwner {
 	
 	// Pet 주인이 소유한 동물 
+	@Autowired //Wiring by Type
+	@Qualifier(value="qf_cat") //AnimalType이 Dog, Cat 2가지여서 명시해줘야함.
+//	@Resource // Wiring by Name
 	private AnimalType animal;
-
-	public PetOwner(AnimalType animal) {
-		super();
-		this.animal = animal;
-	}
+	
+	// Use XML
+//	public PetOwner(AnimalType animal) {
+//		super();
+//		this.animal = animal;
+//	}
 	
 	public void play() {
 		animal.sound();
