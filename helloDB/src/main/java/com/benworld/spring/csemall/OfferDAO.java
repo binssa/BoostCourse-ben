81ba870224f5fc521136104b48565715676ac9b2
binssa <bin.sa0823@gmail.com>
2021-07-15 13:03:39 +0900

    Bean Config using Annotation

--- helloDB/src/main/java/com/benworld/spring/csemall/OfferDAO.java
+++ helloDB/src/main/java/com/benworld/spring/csemall/OfferDAO.java
@@ -2,13 +2,17 @@ package com.benworld.spring.csemall;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+@Component("offerDAO") // beans.xml에서 <bean> 설정 불필요 
 public class OfferDAO {
 	
 	private JdbcTemplate jdbcTemplateObject;
 	private DataSource dataSource;
 
+	@Autowired
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
 	}
