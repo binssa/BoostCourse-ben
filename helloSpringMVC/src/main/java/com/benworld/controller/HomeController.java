77fbb72613d6b0706ff17b7b31b29fb29ebb4762
binssa <bin.sa0823@gmail.com>
2021-07-21 22:57:35 +0900

    Spring slf4j logback 기능 추가

--- helloSpringMVC/src/main/java/com/benworld/controller/HomeController.java
+++ helloSpringMVC/src/main/java/com/benworld/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.benworld.controller;
 
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 /**
  * Handles requests for the application home page.
@@ -10,9 +16,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+//	private static final Logger logger = LoggerFactory.getLogger("com.benworld.HomeController");와 같은 기능 
 	// http://localhost:8080/helloSpringMVC/
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String showHome() {
+	public String showHome(Locale locale, Model model) {
+		
+		logger.trace("trace level : Welecom home! The Client locale is {}", locale);
+		logger.debug("debug level : Welecom home! The Client locale is {}", locale);
+		logger.info("info level : Welecom home! The Client locale is {}", locale);
+		logger.warn("warn level : Welecom home! The Client locale is {}", locale);
+		logger.error("error level : Welecom home! The Client locale is {}", locale);
 		
 		return "home"; // view's Logical Name
 	}
